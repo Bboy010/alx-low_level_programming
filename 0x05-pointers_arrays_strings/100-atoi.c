f037da597b0b393234179148a11dcb8c759ab6ed
Bboy010 <hkoffianderson@gmail.com>
2022-10-25 17:23:55 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,25 @@
-#include "main.h"
-
-
 /**
- * _atoi - converts a string to an integer
- * @s: the string
- * Return: value
+ * _atoi - funtion that extract numbers from string
+ * @s: address of the string
+ * Return: The number extracted
  */
+
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+	int i, sign = 1;
+	unsigned int n = 0;
 
-	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s ==  '-'
+		if (s[i - 1] == 45)
 			sign *= -1;
-			if (*s == '+')
-			sign *= +1;
-			s++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = n * 10 + (s[i] - 48);
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
 	}
-	while ('0' <= *s && *s <= '9' && *s != '\0')
-	{
-		num = (num * 10) + (*s - '0');
-		s++;
-	}
-	return (num * sign);
+	n *= sign;
+	return (n);
 }
