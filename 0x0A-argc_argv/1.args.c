393c5c63859bd5ff1720bf4d83f5786803f34036
Bboy010 <hkoffianderson@gmail.com>
2022-11-08 14:16:02 -0800

    task 1

--- 0x0A-argc_argv/1.args.c
+++ 0x0A-argc_argv/1.args.c
@@ -7,10 +7,10 @@
 * Return: 0 on success
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-(void) argv;
-printf("%i\n", argc - 1);
+if (argc > 0)
+printf(%dn, argc - 1);
 
 return (0);
 }
