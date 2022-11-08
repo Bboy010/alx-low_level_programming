e2994089d92b46f025dcd58daf70a3ee90edf987
Bboy010 <hkoffianderson@gmail.com>
2022-11-07 21:58:54 -0800

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,94 +1,77 @@
 #include <stdio.h>
 
-#include "main.h"
+#include <stdlib.h>
 
 
 
 /**
  *
- *  * _atoi - converts a string to an integer
+ *  * main - program that adds positive numbers.
  *
- *   * @s: string to be converted
+ *   * @argc: number of arguments
  *
- *    *
+ *    * @argv: array with the arguments
  *
- *     * Return: the int converted from the string
+ *     *
  *
- *      */
+ *      * Return: always 0
+ *
+ *       **/
 
-int _atoi(char *s)
+
+
+int main(int argc, char *argv[])
 
 {
 
-		int i, d, n, len, f, digit;
+		int i, suma = 0, res = 0;
+
+			char c[] = "Error", *find_letter;
 
 
 
-			i = 0;
+				if (argc > 1)
 
-				d = 0;
+						{
 
-					n = 0;
-
-						len = 0;
-
-							f = 0;
-
-								digit = 0;
-
-
-
-									while (s[len] != '\0')
-
-												len++;
-
-
-
-										while (i < len && f == 0)
+									for (i = 1; i < argc; i++)
 
 												{
 
-															if (s[i] == '-')
+																find_letter = argv[i];
 
-																			++d;
+																			while (*find_letter != 0)
 
+																							{
 
+																												if (*find_letter < 47 || *find_letter > 57)
 
-																	if (s[i] >= '0' && s[i] <= '9')
+																																	{
 
-																				{
+																																							printf("%s\n", c);
 
-																								digit = s[i] - '0';
+																																												return (1);
 
-																											if (d % 2)
+																																																}
 
-																																digit = -digit;
+																																find_letter++;
 
-																														n = n * 10 + digit;
+																																			}
 
-																																	f = 1;
+																						res = atoi(argv[i]);
 
-																																				if (s[i + 1] < '0' || s[i + 1] > '9')
+																									suma += res;
 
-																																									break;
+																											}
 
-																																							f = 0;
+											printf("%d\n", suma);
 
-																																									}
+												}
 
-																			i++;
+					else
 
-																				}
+								printf("%d\n", 0);
 
-
-
-											if (f == 0)
-
-														return (0);
-
-
-
-												return (n);
+						return (0);
 
 }
-
