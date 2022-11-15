357887ca15d4b3e197f82ff639fd5aaaf42e3e26
Bboy010 <hkoffianderson@gmail.com>
2022-11-15 14:44:52 -0800

    Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,10 +3,10 @@
 
 /*
  * Auth: HonGo
- * Des: Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359
-*/
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
 
 #define PI 3.14159265359
 
 #endif
-
